ea3e7d414d81588cdb1bb4113439d9e00f880420
Hakan KUMAŞ <hakankumas54@gmail.com>
2024-10-31 20:34:55 +0300

    Added delete action for UserPostListItem.

--- client/src/components/UserPostListItem.tsx
+++ client/src/components/UserPostListItem.tsx
@@ -1,6 +1,12 @@
 import { FaEdit } from "react-icons/fa";
 import { MdDelete } from "react-icons/md";
-import { ContentType } from "../types/ContentTypes";
+import { ContentById, ContentType } from "../types/ContentTypes";
+import UserPostListItemDelete from "./UserPostListItemDelete";
+import { useState } from "react";
+import useCustomSnackBar from "../hooks/useCustomSnackBar";
+import { AppDispatch } from "../redux/app/store";
+import { useDispatch } from "react-redux";
+import { delete_content } from "../redux/features/content/contentSlice";
 
 interface UserPostListItemProps {
     content: ContentType;
@@ -8,7 +14,29 @@ interface UserPostListItemProps {
 
 function UserPostListItem({ content }: UserPostListItemProps) {
     console.log("UserPostListItem component rendered.");
-    const { content: dataContent, user } = content;
+
+    const { showSnackBar } = useCustomSnackBar();
+    const dispatch = useDispatch<AppDispatch>();
+    const { content: dataContent, user, _id } = content;
+    const [deleteModal, setDeleteModal] = useState<boolean>(false);
+
+    const handleDelete = async () => {
+        try {
+            const payload: ContentById = {
+                _id,
+            };
+            const response = await dispatch(delete_content(payload)).unwrap();
+            showSnackBar({
+                message: "Successfully Deleted!",
+                variant: "success",
+            });
+        } catch (error) {
+            showSnackBar({
+                message: error as string,
+                variant: "error",
+            });
+        }
+    };
     return (
         <div className="flex items-center border gap-5 my-3 mx-3 p-3 hover:bg-slate-600 rounded-md">
             <div className="w-5/6 flex gap-5">
@@ -24,8 +52,16 @@ function UserPostListItem({ content }: UserPostListItemProps) {
                     <FaEdit className="hover:text-green-400 hover:bg-slate-700 rounded-md p-1 text-3xl" />
                 </button>
                 <button className="w-1/2 flex justify-center items-center">
-                    <MdDelete className="hover:text-red-500 hover:bg-slate-700 rounded-md p-1 text-3xl" />
+                    <MdDelete
+                        className="hover:text-red-500 hover:bg-slate-700 rounded-md p-1 text-3xl"
+                        onClick={() => setDeleteModal(true)}
+                    />
                 </button>
+                <UserPostListItemDelete
+                    deleteModal={deleteModal}
+                    setDeleteModal={setDeleteModal}
+                    handleDelete={handleDelete}
+                />
             </div>
         </div>
     );
